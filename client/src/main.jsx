5f492b4c9147a7b6161fb242a89fe7edeecc9601
Prinsal-Patel <patelprinsal17@gmail.com>
2026-03-24 18:29:17 +0530

    Update Google Client ID for testing purposes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 // Using a standard public dummy client ID or expect it from env
 // Note: For a real app, this should be in .env.local as VITE_GOOGLE_CLIENT_ID
-const GOOGLE_CLIENT_ID = "61647416952-nkh6u9g4h6a0r5m2i3t4s1q0v6b9o4n1.apps.googleusercontent.com"; // Provide a valid looking mock/test ID
+const GOOGLE_CLIENT_ID = "560778091728-a5kaotdo64koqp99s3j647nl5a1d70fh.apps.googleusercontent.com"; // Provide a valid looking mock/test ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
